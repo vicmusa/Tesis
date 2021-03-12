88adf5075a37c9e7bc4fb0d83fcf9dd056a763b4
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-03-12 12:04:58 -0400

    Feat: Agregar SPO2

--- Tesis.ino
+++ Tesis.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heltec.h"
+#include "spo2_algorithm.h"
 #include "heartRate.h"
 
 MAX30105 particleSensor;
@@ -13,10 +14,19 @@ float promtemp; //Promedio Temperatura
 const byte RATE_SIZE = 8; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
-float Spo2;
+bool flag=true;
+
+/* Variables SPO2 & HR*/
 float beatsPerMinute;
-int beatAvg;
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+
 
 void setupADC()
 {
@@ -39,6 +49,17 @@ void setupMAX30102()
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
+  /* Aqui va lo del SPO2*/
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
 }
 void leerADC()
 {
@@ -54,8 +75,7 @@ void leerADC()
 
 void Spo2andHR()
 {
-
- 
+/* 
   long irValue = particleSensor.getIR();
 
 
@@ -77,8 +97,6 @@ void Spo2andHR()
       for (byte x = 0 ; x < RATE_SIZE ; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
-    
-     
   }
   
   
@@ -90,29 +108,101 @@ void Spo2andHR()
   Serial.print(", Avg BPM=");
   Serial.print(beatAvg);
   Serial.println();
+  */
+
+/* SPo2  */
+
+bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+ if(flag)
+ {
+  //lee los primeros 100 samples y determina ek rango de la sena
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+    particleSensor.check(); //Check the sensor for new data
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+ flag=false;
+ //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+ }
+ 
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 seconnd
+  //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+  for (byte i = 25; i < 100; i++)
+  {
+  redBuffer[i - 25] = redBuffer[i];
+  irBuffer[i - 25] = irBuffer[i];
+  }
+   //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+  {
+  while (particleSensor.available() == false) //do we have new data?
+  particleSensor.check(); //Check the sensor for new data
+  //digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+
 }
+}
+
+
+
 
 void pantalla()
 {
 Heltec.display->clear();
-Heltec.display -> drawString(6,40,"BPM: "+String(beatAvg));
+Heltec.display -> drawString(6,40,"BPM: "+String(heartRate));
 Heltec.display -> drawString(0,0,"ID=1");
 Heltec.display -> drawString(6,50,"TEMP: "+String(promtemp));
-Heltec.display -> drawString(6,30,"SPO2: "+String(Spo2));
+Heltec.display -> drawString(6,30,"SPO2: "+String(spo2));
 Heltec.display ->display();
 }
 
 void sendLoRa(void *parameter)
 {
+  delay(4000);
   while(1)
   {
   LoRa.beginPacket();
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
   LoRa.print(promtemp);
   LoRa.print("#");
-  LoRa.print(beatAvg);
+  LoRa.print(heartRate);
   LoRa.print("$");
-  LoRa.print(Spo2);
+  LoRa.print(spo2);
   LoRa.print("/");
   LoRa.print("ID=1");
   LoRa.endPacket();
@@ -150,5 +240,5 @@ void loop()
 leerADC();
 Spo2andHR();
 pantalla();
-delay(10);
+delay(500);
 }
